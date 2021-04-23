ebaa0bd7f52a4c601f7fba02696708e179a7b008
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-23 18:19:38 +0000

    fixed a bug where "previousOperand" would display "undefined"

--- app.js
+++ app.js
@@ -1,78 +1,69 @@
-// Class constructor for the Calculator
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement){   /*Takes the current and previous values */
+    constructor(previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
-        this.clear()  /* calls clear method every time the calculator is initialized */
+        this.clear();
     }
 
-    // Clears both values and the operation symbol 
     clear() {
-            this.currentOperand = ''
-            this.previousOperandText = ''
-            this.operation = undefined
+            this.currentOperand = '';
+            this.previousOperand = '';
+            this.operation = undefined;
     }
- // Delete button that removes the latest character of the currentOperand string 
+
     delete() {
             this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
-// insert a number value //
+
     appendNumber(number) {
-        if(number === '.' && this.currentOperand.includes('.')) return   // prevents from having many '.' characters in one number //
-        this.currentOperand = this.currentOperand.toString() + number.toString();  // adds the number next to the already existing value on the active (current) slot
+        if(number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
-// inserts the kind of operation to apply on the numbers //
     chooseOperation(operation) {
-        if(this.currentOperand === '') return  // if the current slot is empty, and a symbol is picked, nothing happens
-        if(this.previousOperand !== '') {  // if the previousOperand carries a value, call computing method 
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
             this.compute()
         }
-        this.operation = operation;   // for the compute method 
-        this.previousOperand = this.currentOperand; // copies the current to the previous slot
-        this.currentOperand = ''; // empties the current
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
 // Compute single value 
     compute() {
-    let computation = 0;  // variable for the result of the operation
-    const prev = parseFloat(this.previousOperand); // turns the previous to a number
-    const current = parseFloat(this.currentOperand); // turns current to a number
-    if (isNaN(prev) || isNaN(current)) return // if one of the two is not a number, do nothing
+    let computation
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return
     switch (this.operation) {
         case '+':
-            computation = prev + current;
+            computation = prev + current
             break
             case '-':
-            computation = prev - current;
+            computation = prev - current
             break
             case '*':
-            computation = prev * current;
+            computation = prev * current
             break
             case '÷':
-            computation = prev / current;
+            computation = prev / current
             break
-            case '√':
-                computation = Math.sqrt(current);
-                break
             default: 
             return
     }
-    this.currentOperand = computation;
-    this.operation = undefined;
-    this.previousOperand = '';
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
     }
-   
 
     updateDisplay() {
         this.currentOperandTextElement.innerText = this.currentOperand;
         this.previousOperandTextElement.innerText = this.previousOperand;
-        
     }
 }
 
 
 const numberButtons = document.querySelectorAll('[data-number]');
-const pie = document.querySelector('[data-pi]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
@@ -89,12 +80,6 @@ numberButtons.forEach(button => {
         calculator.updateDisplay();
     })
 })
-
-pie.addEventListener('click', () => {
-    calculator.appendNumber(22/7);
-    calculator.updateDisplay();
-})
-
 
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
